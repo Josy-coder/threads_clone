7777bc47e77a0f163698cb56a5a529e21080ee82
Josy-coder <josekecha@keemail.me>
2023-10-13 18:36:18 +0200

    modified topbar to light background and added a logo

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar() {
-    return <h1>Topbar</h1>
+    return (
+        <nav className="topbar">
+            <Link href="/" className="flex ites-center gap-4">
+                <Image src="/assets/logo_uok.png" alt="logo" width={100} height={50} />
+                <p className="text-heading3-bold text-dark-1 max-xs:hidden">
+                    Forum
+                </p>
+            </Link>
+        </nav>
+    )
 }
 
 export default Topbar;
