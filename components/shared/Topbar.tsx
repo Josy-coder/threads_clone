e1213a1d96672ee33f20ab4bbac129cc05ec70a7
Josy-coder <josekecha@keemail.me>
2023-10-24 18:21:26 +0200

    added constants index.js file

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,9 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 function Topbar () {
+    const isUserLoggedIn = true
     return (
         <nav className="topbar">
             <Link href="/" className="flex items-center gap-4">
@@ -10,6 +12,31 @@ function Topbar () {
                     Threads
                 </p>
             </Link>
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className="flex cursor-pointer">
+                                <Image
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24}
+                                />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+
+                </div>
+                <OrganizationSwitcher
+                    appearance={{
+                        elements: {
+                            organizationSwitcherTrigger:
+                            "py-2 px-4"
+                        }
+                    }}
+                />
+            </div>
         </nav>
     )
 }
